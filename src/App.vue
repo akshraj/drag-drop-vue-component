896b7b104c5fb31599ed6712a473c2279fc44168
akshayraj <akshay.singh@thepsi.com>
2022-07-07 14:51:09 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">File: {{ dropzoneFile.name }}</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DropZone from '@/components/DropZone.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DropZone
+  },
+  setup() {
+    let dropzoneFile = ref('');
+
+    function drop(e) {
+      dropzoneFile.value = e.dataTransfer.files[0];
+    }
+
+    function selectedFile(e) {
+      // dropzoneFile.value = document.querySelector('.dropzoneFile').files[0];
+      dropzoneFile.value = e.target.files[0];
+    }
+
+    return { dropzoneFile, drop, selectedFile }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.home {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+
+  h1 {
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+
+  .file-info {
+    margin-top: 32px;
+  }
+}
+</style>
